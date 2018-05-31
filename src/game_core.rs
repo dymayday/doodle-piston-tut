ac153afbc8c95dff628893fb159c2dc8aa32fb49
meidhy <dymayday@gmail.com>
2018-05-31 19:46:21 +0200

    add color const as a mod

---
+++ src/game_core.rs
@@ -0,0 +1,13 @@
+
+use opengl_graphics::GlGraphics;
+
+use color_picker::{BLACK, BLUE};
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64   // Rotation for the square.
+}
+
+impl App {
+    fn render(&mut self, )
+}
---
+++ src/mod.rs
@@ -0,0 +1,2 @@
+pub self::color_picker;
+pub self::game_core;
